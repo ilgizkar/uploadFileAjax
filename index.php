7a0836d30be861465d621cf9a09f464a88e275d2
ilgizkar <karabaev.il@yandex.ru>
2019-12-09 23:06:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-wysihtml5.css">
-    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
-    <script src="bootstrap/js/wysihtml5-0.3.0.js"></script>
-    <script src="bootstrap/js/jquery-3.4.1.min.js"></script>
-    <script src="bootstrap/js/bootstrap.js"></script>
-    <script src="bootstrap/js/bootstrap-wysihtml5.js"></script>
+
 
     <title>Document</title>
 </head>
